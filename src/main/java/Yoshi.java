38635b01f28315e2ce6e9e37fc2cf7c4643edba0
edwardrl101 <edward.rl101@gmail.com>
2025-01-31 14:11:25 +0800

    Add ability to add and list down tasks

--- src/main/java/Yoshi.java
+++ src/main/java/Yoshi.java
@@ -1,6 +1,21 @@
 import java.util.Scanner;
 
 public class Yoshi {
+
+    public static void addTask(String[] tasks, int numTasks, String line) {
+        tasks[numTasks] = line;
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     added: " + line);
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void listTasks(String[] tasks, int numTasks) {
+        System.out.println("    ____________________________________________________________");
+        for (int i = 1; i <= numTasks; i++) {
+            System.out.println("    " + i +". " + tasks[i-1]);
+        }
+        System.out.println("    ____________________________________________________________");
+    }
     public static void main(String[] args) {
         System.out.println("    ____________________________________________________________");
         System.out.println("    Hello! I'm Yoshi!");
@@ -15,15 +30,19 @@ public class Yoshi {
         System.out.println("    ____________________________________________________________");
 
         Scanner in = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int numTasks = 0;
 
-        while(true) {
+        while(numTasks < 100) {
             String line = in.nextLine();
             if(line.equals("bye")) {
                 break;
+            } else if(line.equals("list")) {
+                listTasks(tasks, numTasks);
+            } else {
+                addTask(tasks, numTasks, line);
+                numTasks++;
             }
-            System.out.println("    ____________________________________________________________");
-            System.out.println("    " + line);
-            System.out.println("    ____________________________________________________________");
         }
 
         System.out.println("    ____________________________________________________________");

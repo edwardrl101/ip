985128de7ff58fb3521be0e8ec303ce31301949c
edwardrl101 <edward.rl101@gmail.com>
2025-02-03 03:58:58 +0800

    Add support for three more types of tasks

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -26,4 +26,9 @@ public class Task {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
 }
--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -8,14 +8,52 @@ public class TaskManager {
         this.numTasks = 0;
     }
 
+    public Todo initializeTodo(String line) {
+        return new Todo(line);
+    }
+
+    public Deadline initializeDeadline(String line) {
+        String[] taskParts = line.split("/by", 2);
+        String activity = taskParts[0].trim();
+        String by = taskParts[1].trim();
+        return new Deadline(activity, by);
+    }
+
+    public Event initializeEvent(String line) {
+        String[] taskParts = line.split("/from", 2);
+        String activity = taskParts[0].trim();
+        String[] dates = taskParts[1].split("/to", 2);
+        String from = dates[0].trim();
+        String to = dates[1].trim();
+        return new Event(activity, from, to);
+    }
+
     public void addTask(String line) {
         System.out.println("    ____________________________________________________________");
         if(numTasks >= MAX_TASKS) {
             System.out.println("    Oops! Unable to add task: your task list is full!");
         } else {
-            tasks[numTasks] = new Task(line);
-            System.out.println("     added: " + line);
+            String[] taskParts = line.split(" ", 2);
+            String taskType = taskParts[0].trim();
+            String taskDetails = taskParts[1].trim();
+            switch(taskType) {
+            case "todo":
+                tasks[numTasks] = initializeTodo(taskDetails);
+                break;
+            case "deadline":
+                tasks[numTasks] = initializeDeadline(taskDetails);
+                break;
+            case "event":
+                tasks[numTasks] = initializeEvent(taskDetails);
+                break;
+            default:
+                tasks[numTasks] = new Task(line);
+                break;
+            }
+            System.out.println("    Sure! I've added this task:");
+            System.out.println("     " + tasks[numTasks]);
             numTasks++;
+            System.out.println("    You now have " + numTasks + " tasks in your list!");
         }
         System.out.println("    ____________________________________________________________");
     }
@@ -24,7 +62,7 @@ public class TaskManager {
         System.out.println("    ____________________________________________________________");
         System.out.println("     Here are the tasks in your list:");
         for (int i = 1; i <= numTasks; i++) {
-            System.out.println("     " + i +".[" + tasks[i-1].getStatusIcon() +"] " + tasks[i-1].getDescription());
+            System.out.println("     " + i +"." + tasks[i-1]);
         }
         System.out.println("    ____________________________________________________________");
     }
@@ -38,11 +76,11 @@ public class TaskManager {
         } else if (command.equals("mark")) {
             tasks[taskNumber].markAsDone();
             System.out.println("     Great job! I've marked this task as done:");
-            System.out.println("      [" + tasks[taskNumber].getStatusIcon() + "] " + tasks[taskNumber].getDescription());
+            System.out.println("      " + tasks[taskNumber]);
         } else {
             tasks[taskNumber].unmarkAsDone();;
             System.out.println("     Sure! I've marked this task as not done yet:");
-            System.out.println("      [" + tasks[taskNumber].getStatusIcon() + "] " + tasks[taskNumber].getDescription());
+            System.out.println("      " + tasks[taskNumber]);
         }
         System.out.println("    ____________________________________________________________");
     }
